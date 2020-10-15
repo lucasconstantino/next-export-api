8174b6c87e127ef34f6a3e08275e0fe72684f8f7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-10-16 01:32:39 +0200

    test: added adaptor tests

--- test/adaptor.test.ts
+++ test/adaptor.test.ts
@@ -5,6 +5,7 @@ describe('adaptor', () => {
     ok: (req, res) => res.send('ok'),
     status: (req, res) => res.status(201).send('ok'),
     query: (req, res) => res.send(req.query.name),
+    post: (req, res) => res.send('ok'),
     body: (req, res) => res.send(req.body.name),
     json: (req, res) => res.json({ message: 'ok' }),
   }
@@ -13,17 +14,19 @@ describe('adaptor', () => {
     ok: jest.fn(handlers.ok),
     status: jest.fn(handlers.status),
     query: jest.fn(handlers.query),
+    post: jest.fn(handlers.post),
     body: jest.fn(handlers.body),
     json: jest.fn(handlers.json),
   }
 
   const adaptors = (netlify: boolean) => {
-    process.env.NETLIFY = netlify ? 'true' : null
+    process.env.NETLIFY = netlify ? 'true' : ''
 
     return {
       ok: adaptor(spies.ok),
       status: adaptor(spies.status),
       query: adaptor(spies.query),
+      post: adaptor(spies.post),
       body: adaptor(spies.body),
       json: adaptor(spies.json),
     }
@@ -31,8 +34,10 @@ describe('adaptor', () => {
 
   beforeEach(jest.clearAllMocks)
 
-  describe('Next.js', () => {
-    const { ok } = adaptors(false) as { [key: string]: NetlifyApiHandler }
+  describe('Netlify', () => {
+    const { ok, status, query, post, body, json } = adaptors(true) as {
+      [key: string]: NetlifyApiHandler
+    }
 
     test('"ok" handler', async () => {
       const result = await ok(
@@ -41,6 +46,62 @@ describe('adaptor', () => {
       )
 
       expect(result.body).toEqual('ok')
+    })
+
+    test('"status" handler', async () => {
+      const result = await status(
+        { httpMethod: 'GET', path: '/' } as any,
+        {} as any
+      )
+
+      expect(result.statusCode).toEqual(201)
+      expect(result.body).toEqual('ok')
+    })
+
+    test('"query" handler', async () => {
+      const result = await query(
+        {
+          httpMethod: 'GET',
+          path: '/',
+          queryStringParameters: { name: 'susan' },
+        } as any,
+        {} as any
+      )
+
+      expect(result.body).toEqual('susan')
+    })
+
+    test('"post" handler', async () => {
+      const result = await post(
+        { httpMethod: 'POST', path: '/' } as any,
+        {} as any
+      )
+
+      expect(result.body).toEqual('ok')
+    })
+
+    test('"body" handler', async () => {
+      const result = await body(
+        {
+          httpMethod: 'POST',
+          path: '/',
+          headers: { 'Content-Type': 'application/json' },
+          body: '{ "name": "susan" }',
+        } as any,
+        {} as any
+      )
+
+      expect(result.body).toEqual('susan')
+    })
+
+    test('"json" handler', async () => {
+      const result = await json(
+        { httpMethod: 'GET', path: '/' } as any,
+        {} as any
+      )
+
+      expect(result.headers['content-type']).toMatch('application/json')
+      expect(result.body).toEqual('{"message":"ok"}')
     })
   })
 })
